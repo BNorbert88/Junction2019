f1f0f985301a926f5366ec0c2215ffbb4b327586
Nyirő Péter <scorpeti@gmail.com>
2019-10-26 02:35:59 +0200

    final script of downloading raw data

--- cstreamer.py
+++ cstreamer.py
@@ -19,6 +19,9 @@ class CStreamer:
         'station': "https://api.hypr.cl/station/"
     }
 
+    streamed = []
+    raw_array = []
+
     def get_stations(self):
         header = self.HEADERS_COMMON.copy()
         header.update({
@@ -26,21 +29,33 @@ class CStreamer:
         })
         return json.loads(requests.request("POST", self.URLS['station'], headers=header).text)
 
-    def get_rawdata(self, start: datetime, end: datetime):
+    def rawdata_stream(self, start: datetime, end: datetime, step: timedelta):
         if end < start:
             raise Exception('End time should be not smaller than start time')
 
         def daterange(start_datetime, end_datetime):
-            for n in range(int((end_datetime - start_datetime).seconds)):
-                yield start_datetime + timedelta(seconds=n)
+            for n in range(int(((end_datetime - start_datetime).seconds + (end_datetime - start_datetime).days * 60*60*24) / (step.seconds + step.days * 60*60*24))):
+                yield start_datetime + step * n
 
-        raw_array = []
         for dt in daterange(start, end):
+            self._get_one_rawdata(dt)
+        return {
+            'streamed': self.streamed,
+            'raw': self.raw_array
+        }
+
+    def _get_one_rawdata(self, dt):
+        time = dt.replace(microsecond=0).isoformat() + 'Z'
+        if time not in self.streamed:
             header = self.HEADERS_COMMON.copy()
+
             header.update({
-                'time_start': dt.replace(microsecond=0).isoformat()+'Z',
-                'time_stop': dt.replace(microsecond=0).isoformat()+'Z'
+                'time_start': time,
+                'time_stop': time
             })
             response = json.loads(requests.request("POST", self.URLS['raw'], headers=header).text)
-            raw_array.extend(response['raw'])
-        return raw_array
+            self.raw_array.extend(response['raw'])
+            self.streamed.append(time)
+
+            with open('rawdata_8', 'a') as file:
+                file.write(str(response['raw']) + '\n')
--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from cstreamer import CStreamer
 
+import numpy as np
 
 streamer = CStreamer()
 stations = streamer.get_stations()
-raw = streamer.get_rawdata(start=datetime(2019, 7, 15, 0, 0, 0), end=datetime(2019, 7, 15, 0, 1, 0))
-
-streamed = []
-
-print(raw)
+rawdata = streamer.rawdata_stream(
+    start=datetime(2019, 8, 5, 0, 0, 0),
+    end=datetime(2019, 8, 6, 0, 0, 0),
+    step=timedelta(seconds=120))
